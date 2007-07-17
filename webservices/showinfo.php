0acea04d1f7ab3b3cbf3f347328c5db0e7dd91ae
rulecam <rulecam@ab8e8014-d70f-0410-b893-9bef6f55f187>
2007-07-17 09:49:21 +0000

    - showinfo shows more details - added shows: Hell's Kitchen The Closer Greek The Simple Life Age of Love Heartland America's Got Talent American Inventor Last Comic Standing Ghost Hunter Top Chef Psych The Loop The Dead Zone Rescue Me Standoff The Tonight Show with Jay Leno Late Night with Conan O'Brien
    git-svn-id: https://ted.svn.sourceforge.net/svnroot/ted/trunk@324 ab8e8014-d70f-0410-b893-9bef6f55f187

--- webservices/showinfo.php
+++ webservices/showinfo.php
@@ -11,7 +11,9 @@
 	$tvcom = $_GET["tvcom"];
 	$description = "";
 	
-	$tvcom_content = getPageContent($tvcom);
+	$tvcom_url="http://www.tv.com/show/" . $tvcom . "/summary.html";
+	
+	$tvcom_content = getPageContent($tvcom_url);
 	
 	//foreach ($tvcom_content as $regel)
 	//{
@@ -30,10 +32,16 @@
 				$description = $pieces[2];
 			}
 			
+			$tempstatus = strstr ($tvcom_content, "<div class=\"f-bold f-666 f-11\">");
+			$endpos = strpos ($tempstatus, "<div class=\"mt-10\">");
+			$tempstatus = substr ($tempstatus, 0, $endpos);
+			$tempstatus = str_replace("</span>", "<br>", $tempstatus);
+			$tempstatus = str_replace("<span class="f-333">", "", $tempstatus);
+			
 			$tempim = strstr($tvcom_content, "<a class=\"default-image more");
 			$endpos = strpos ($tempim, "</a>");
 			$tempim = substr ($tempim, 0, $endpos+4);
-			$tempim = str_replace("More Pictures", "", $tempim);
+			//$tempim = str_replace("More Pictures", "", $tempim);
 			
 			// change \> with >
 			$description = str_replace("/>", ">", $description);
@@ -63,7 +71,24 @@
 	}
 
 ?>
-<table border="0" align="left"><tr valign="top"><td valign="top"><?echo($tempim);?></td><td valign="top"><font color="#000000" size="3" face="Arial, Helvetica, sans-serif"><?echo($description);?></font></td></tr></table>
+<table border="0" align="left">
+<tr valign="top">
+	<td align="left" valign="top" bgcolor="#CCCCCC">
+		<font color="#000000" size="2" face="Arial, Helvetica, sans-serif">
+			<?echo($tempim);?><br>
+			<a href="<?echo($tvcom_url)?>">copyright TV.com</a><br>
+			<?echo($tempstatus)?>
+		</font>
+	
+	
+	</td>
+	<td valign="top">
+		<font color="#000000" size="3" face="Arial, Helvetica, sans-serif">
+			<?echo($description);?>
+		</font>
+	</td>
+</tr>
+</table>
 
 </body>
 </html>
