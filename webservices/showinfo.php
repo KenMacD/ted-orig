7163fdd3465cea53c9b907f2764271474e102425
rulecam <rulecam@ab8e8014-d70f-0410-b893-9bef6f55f187>
2007-10-07 20:29:17 +0000

    - button location is different per UI - cleanup of growl code - showinfo modified so it uses curl instead of fopen
    git-svn-id: https://ted.svn.sourceforge.net/svnroot/ted/trunk@392 ab8e8014-d70f-0410-b893-9bef6f55f187

--- webservices/showinfo.php
+++ webservices/showinfo.php
@@ -73,7 +73,7 @@
 	function getPageContent($url)
 	{
 		//$tempVar = "";
-		$page = "";
+		/*$page = "";
 		if($url != ""){
 			$tempVar = file($url);
 			foreach ($tempVar as $regel)
@@ -84,7 +84,33 @@
 		}else{
 			$tempVar = "No valid url passed!";
 		}
-		return $tempVar;
+		return $tempVar;*/
+		$curl = curl_init();
+
+	  // Setup headers - I used the same headers from Firefox version 2.0.0.6
+	  // below was split up because php.net said the line was too long. :/
+	  $header[0] = "Accept: text/xml,application/xml,application/xhtml+xml,";
+	  $header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
+	  $header[] = "Cache-Control: max-age=0";
+	  $header[] = "Connection: keep-alive";
+	  $header[] = "Keep-Alive: 300";
+	  $header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
+	  $header[] = "Accept-Language: en-us,en;q=0.5";
+	  $header[] = "Pragma: "; // browsers keep this blank.
+	
+	  curl_setopt($curl, CURLOPT_URL, $url);
+	  curl_setopt($curl, CURLOPT_USERAGENT, 'Googlebot/2.1 (+http://www.google.com/bot.html)');
+	  curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+	  curl_setopt($curl, CURLOPT_REFERER, 'http://www.google.com');
+	  curl_setopt($curl, CURLOPT_ENCODING, 'gzip,deflate');
+	  curl_setopt($curl, CURLOPT_AUTOREFERER, true);
+	  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	  curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+	
+	  $html = curl_exec($curl); // execute the curl command
+	  curl_close($curl); // close the connection
+	
+	  return $html; // and finally, return $html
 	}
 
 ?>
