90fcfa9944f2446196f478ff2245fcb8af6947e6
ted_jofo <ted_jofo@ab8e8014-d70f-0410-b893-9bef6f55f187>
2008-01-13 13:55:58 +0000

    Save window settings on resize.
    git-svn-id: https://ted.svn.sourceforge.net/svnroot/ted/trunk@421 ab8e8014-d70f-0410-b893-9bef6f55f187

--- ted/TedMainDialog.java
+++ ted/TedMainDialog.java
@@ -3,6 +3,8 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
@@ -259,6 +261,16 @@ public class TedMainDialog extends javax.swing.JFrame implements ActionListener
 					}
 					
 					rootWindowDeiconified(evt);
+				}
+			}
+		);
+		
+		// Save the window settings on resize.
+		this.addComponentListener(new ComponentAdapter()
+			{
+				public void componentResized(ComponentEvent evt)
+				{
+					saveConfig(false);
 				}
 			}
 		);
