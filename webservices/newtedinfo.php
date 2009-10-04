fe2a3c2aeb88a50468c0245e02749edb5d6c0dbf
ted_jofo <ted_jofo@ab8e8014-d70f-0410-b893-9bef6f55f187>
2009-10-04 15:25:06 +0000

    v0.97 info screen
    git-svn-id: https://ted.svn.sourceforge.net/svnroot/ted/trunk@818 ab8e8014-d70f-0410-b893-9bef6f55f187

--- webservices/newtedinfo.php
+++ webservices/newtedinfo.php
@@ -10,20 +10,69 @@
 <table width="95%" border="0" cellpadding="5">
   <tr>
     <td width="100" align="left"><a href="http://www.ted.nu/"><img src="http://www.ted.nu/img/header_logo.png" alt="ted logo" width="98" height="85" border="0"></a></td>
-    <td align="left" valign="middle"><p><font face="Arial, Helvetica, sans-serif" size="+1">new in version 0.96</font></p>
-    <p><font face="Arial, Helvetica, sans-serif" size="-1">released: 20-10-2009</font></p></td>
+    <td align="left" valign="middle"><p><font face="Arial, Helvetica, sans-serif" size="+1">new in version 0.97</font></p>
+    <p><font face="Arial, Helvetica, sans-serif" size="-1">released: 04-10-2009</font></p></td>
   </tr>
   <tr>
     <td colspan="2">
     	<font face="Arial, Helvetica, sans-serif" size="-1">
-    	<strong><font color="#225A8D" size="+1" face="Arial, Helvetica, sans-serif">Bug Fixes</font></strong>      
-        <ul type="disc">
-          <li>Parse shows did not work at start of ted on Windows OS</li>
-          <li>Open torrent files in torrent client when torrent path contains spaces on MacOS</li>
-          <li>Update status bar count down when adjusting time between parse rounds</li>
-          <li>Details of shows are no longer overwritten at the start of ted</li>
-    	</ul>
-        </font>
+		<p>
+		<strong><font color="#225A8D" size="+1" face="Arial, Helvetica, sans-serif">New features</font></strong>
+		</p>
+		<ul><li>Support for HD shows.
+		</li><li>Sort shows on name or air date.
+		</li><li>EZRss feed parser support.
+		</li><li>Proxy support via new network preferences tab.
+		</li><li>Edit multiple shows at once.
+
+		</li><li>Nicer air dates in main dialog (tomorrow, today, ...).
+		</li><li>Seeders and file sizes are taken from rss feed before contacting tracker. Makes parsing faster and more robust.
+		</li><li>Filter on private trackers option
+		</li><li>Option to update show schedule in edit show dialog.
+		</li><li>yyyy mm dd is added as a valid date.
+		</li><li>Added window that shows information about new ted or show list updates.
+		</li><li>Adjustments to the translator so it is easier to work with
+		</li><li>Changed rss library because of extensive logging
+		</li><li>In/export shows function support
+		</li><li>Added never/always buttons to update pop up dialogs
+		</li><li>Help button on tabs dialogs points to correct help page
+		</li></ul>
+		<p><br />
+		<strong><font color="#225A8D" size="+1" face="Arial, Helvetica, sans-serif">Bug fixes</font></strong>
+		</p>
+		<ul><li>Tray icon disabled on 64 bits machines so ted starts.
+
+		</li><li>Fix for lost shows after adding show and restart.
+		</li><li>Show list is downloaded when not present.
+		</li><li>Schedule parser fix when season 0 is present.
+		</li><li>Adjusted default download folders for different OS.
+		</li><li>If download folder not present create it (otherwise warn user).
+		</li><li>Wild clicking in add show dialog caused inconsistent data.
+		</li><li>Rejected torrents because of keywords show up in log.
+		</li><li>When the directory path was too long UI of preferences menu was disarranged.
+		</li><li>Renamed compressed file filter into generic file filter.
+		</li><li>Fixed allowing '=' in the config (directory path).
+		</li><li>Right mouse click in main table doesn't need focus of main table.
+		</li><li>Message that dis/enable tray icon needs reboot.
+		</li><li>Linux tray support.
+		</li><li>Fixed some epguides parsing problems.
+		</li><li>Don't crash when xml file from TVRage can't be downloaded.
+		</li><li>Fixed bug in synchronizer show definitions.
+		</li><li>The characters [ and ] are invalid in torrent name.
+
+		</li><li>Don't increase episode number when torrent can't be downloaded.
+		</li><li>Don't always update schedule when closing edit show dialog.
+		</li><li>Some connection time outs were hard coded now linked to config.
+		</li><li>Add show dialog has a title.
+		</li></ul>
+		<p>
+		<strong><font color="#225A8D" size="+1" face="Arial, Helvetica, sans-serif">Removed</font></strong>
+		</p>
+		<ul><li>Manual schedulers.
+		</li><li>Double episode support.
+		</li></ul>
+		 </font>
+
     </td>
   </tr>
 </table>
